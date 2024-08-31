807f6991833b2aefea64eca37e9de31e62e05d91
Martin Lettry <m.lettry1@gmail.com>
2024-08-31 20:17:39 +0200

    network: fix server not available to other devices on local network

--- run_all.sh
+++ run_all.sh
@@ -7,4 +7,4 @@ python3 manage.py makemigrations tornado_track
 python3 manage.py migrate tornado_track
 
 # Run the server
-python3 manage.py runserver 8000
+python3 manage.py runserver 0.0.0.0:8000
--- tornado_track/tornado_track/settings.py
+++ tornado_track/tornado_track/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hv&a_yen0prp+&qn25r&!uw3x5dei^gwjr3eq^dg&j=@b!ywu6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
